43877c1fb9d6885c3a28fa8adbc890a87310ee16
MacKenzie Rawcliffe <kenziejoy@gmail.com>
2015-11-19 16:17:37 -0800

    reduce number of pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -523,9 +523,9 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-	var cols = 8;
-	var s = 256;
-	for (var i = 0; i < 200; i++) {
+	var cols = 5;
+	var s = 50;
+	for (var i = 0; i < 50; i++) {
 	var elem = document.createElement('img');
 	elem.className = 'mover';
 	elem.src = "images/pizza.png";
