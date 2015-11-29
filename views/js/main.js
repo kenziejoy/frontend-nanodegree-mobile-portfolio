5a5f7bfb4cf2b27aa046c1ef24614823d65f7960
MacKenzie Rawcliffe <kenziejoy@gmail.com>
2015-11-28 22:12:03 -0800

    Updates from review
    Moved var elem and var movingPizzas outside loop. Moved .length
    variables outside change size and update positions

--- views/js/main.js
+++ views/js/main.js
@@ -1,17 +1,3 @@
-/*
-Welcome to the 60fps project! Your goal is to make Cam's Pizzeria website run
-jank-free at 60 frames per second.
-There are two major issues in this code that lead to sub-60fps performance. Can
-you spot and fix both?
-Built into the code, you'll find a few instances of the User Timing API
-(window.performance), which will be console.log()ing frame rate data into the
-browser console. To learn more about User Timing API, check out:
-http://www.html5rocks.com/en/tutorials/webperformance/usertiming/
-Creator:
-Cameron Pittman, Udacity Course Developer
-cameron *at* udacity *dot* com
-*/
-
 // As you may have realized, this website randomly generates pizzas.
 // Here are arrays of all possible pizza ingredients.
 var pizzaIngredients = {};
@@ -506,8 +492,9 @@ function updatePositions() {
 	var items = document.getElementsByClassName("mover");
 	var scrollTopDoc = document.body.scrollTop / 1250;
 	var phase;
+	var itemsLength = items.length;
 
-	for (var i = 0; i < items.length; i++) {
+	for (var i = 0; i < itemsLength; i++) {
 	phase = Math.sin( scrollTopDoc + (i % 5));
 	items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
 	}
@@ -529,15 +516,17 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
 	var cols = 8;
 	var s = 256;
+	var elem;
+	var movingPizzas = document.getElementById('movingPizzas1');
 	for (var i = 0; i < 20; i++) {
-	var elem = document.createElement('img');
+	elem = document.createElement('img');
 	elem.className = 'mover';
 	elem.src = "images/pizza.png";
 	elem.style.height = "100px";
 	elem.style.width = "73.333px";
 	elem.basicLeft = (i % cols) * s;
 	elem.style.top = (Math.floor(i / cols) * s) + 'px';
-	document.querySelector("#movingPizzas1").appendChild(elem);
+	movingPizzas1.appendChild(elem);
 	}
 	updatePositions();
 });
